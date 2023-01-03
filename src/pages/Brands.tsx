07eb15bc549dbb78006506503d39cf21af48865f
Mohammad Khayata <Mohammad_Khayata@outlook.com>
2023-01-03 21:13:27 +0300

    fix

--- src/pages/Brands.tsx
+++ src/pages/Brands.tsx
@@ -5,10 +5,11 @@ import Grid from '@mui/material/Grid'
 import { Box, Button, Card } from '@mui/material'
 import { borderRadius } from '@mui/system'
 import { AppDispatch, RootState } from '../store'
+import { BrandItem } from '@/api/Brand/dto'
 
 
 export default function Brands() {
-  const brands = useSelector<RootState>((state) => state.brand.brands) as []
+  const brands = useSelector<RootState, BrandItem[]>((state) => state.brand.brands) as []
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchBrands());
@@ -16,25 +17,23 @@ export default function Brands() {
 
   return (
     <>
-      <React.Suspense fallback={'Loading >>>'}>
+      <React.Suspense fallback={'Loading '}>
 
-        <div className='grid grid-cols-6 gap-4'>
-          {brands.map((b) => {
+        <div className='grid grid-cols-6 gap-4 p-4'>
+          {brands.map((b: BrandItem) => {
             return (
-              <div>
 
-                <Card className='p-4 flex items-center justify-center relative' sx={{ borderRadius: '15px' }} elevation={5}>
-                  <img width={150} src={`/public/brands/${b.name}.png`} alt="" />
+              <Card key={b.id} className='p-4 flex items-center justify-center relative' sx={{ borderRadius: '15px' }} elevation={5}>
+                <img width={150} src={`/brands/${b.name}.png`} alt="" />
 
-                  <Box sx={{ transition: '0.5s' }} className="absolute h-full w-full brand-info bg-white flex justify-center items-center text-2xl bg-opacity-70 opacity-0 hover:opacity-100 backdrop-blur-sm ">
-                    <Button size='large' color='primary' sx={{ borderRadius: '15px', fontSize: '20px', fontWeight: "bold" }}>
+                <Box sx={{ transition: '0.5s' }} className="absolute h-full w-full brand-info bg-white flex justify-center items-center text-2xl bg-opacity-70 opacity-0 hover:opacity-100 backdrop-blur-sm ">
+                  <Button size='large' color='primary' sx={{ borderRadius: '15px', fontSize: '20px', fontWeight: "bold" }}>
 
-                      عرض القطع
-                    </Button>
-                  </Box>
-                </Card>
+                    عرض القطع
+                  </Button>
+                </Box>
+              </Card>
 
-              </div>
             )
           })}
         </div>
